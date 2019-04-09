904dba108cef739a55ed4a43627ab0d57add6108
kcdporter <kiracdporter@gmail.com>
2019-04-09 18:44:51 +0000

    jsx files are modular, sass is no longer orange

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
 
+import NavBar from './NavBar.jsx';
+import Message from './Message.jsx';
+import ChatBar from './ChatBar.jsx';
+
 class App extends Component {
   render() {
     return (
+      <div>
       <h1>Hello React :)</h1>
+      <NavBar />
+      <Message />
+      <ChatBar />
+      </div>
     );
   }
 }
