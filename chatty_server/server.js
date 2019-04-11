7aeb8245e57862a3554457271533645593818f8f
kcdporter <kiracdporter@gmail.com>
2019-04-11 19:19:37 +0000

    messages are being sent to the server

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const SocketServer = require('ws').Server;
+const webSocket = require('ws');
 
 // Set the port to 3001
 const PORT = 3001;
@@ -11,14 +11,18 @@ const server = express()
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
 // Create the WebSockets server
-const wss = new SocketServer({ server });
+const WebSocketServer = new webSocket.Server({server});
+
 
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
-wss.on('connection', (ws) => {
+WebSocketServer.on('connection', () => {
   console.log('Client connected');
+  WebSocketServer.on( 'message', function incomingMessage(message) {
+    console.log("Server side:", message);
+  })
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  WebSocketServer.on('close', () => console.log('Client disconnected'));
 });
